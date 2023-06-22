eb8f10a3b7f43b33030034ac2dfd26e90e2aac8d
Daniel <d.akipskiy@gmail.com>
2023-06-22 10:59:34 +0800

    Database initialization

---
+++ src/auth/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Boolean
+
+from src.models import Base
+
+
+class User(Base):
+    id: int = Column()
+    email: str = Column(String(length=320), unique=True, index=True, nullable=False)
+    hashed_password: str = Column(String(length=1024), nullable=False)
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
---
+++ src/database.py
@@ -0,0 +1,24 @@
+from typing import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from auth.models import User
+
+# DATABASE_URL = "postgresql://user:password@postgresserver/db"
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost/shift-project-db"
+
+engine = create_engine(DATABASE_URL)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
