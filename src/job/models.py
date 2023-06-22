b73174886dd2bbdeebd2c0fba9bc668eb195eec6
Daniel <d.akipskiy@gmail.com>
2023-06-22 12:41:26 +0800

    add __tablename__ to table's Classes

--- src/job/models.py
+++ src/job/models.py
@@ -4,6 +4,8 @@ from src.models import Base
 
 
 class Job(Base):
+    __tablename__ = "job"
+
     id: int = Column(Integer, primary_key=True, autoincrement=True)
     title: str = Column(String, nullable=False, index=True)
     description: str = Column(String, nullable=True)
--- src/role/models.py
+++ src/role/models.py
@@ -6,6 +6,8 @@ from src.models import Base
 
 
 class Role(Base):
+    __tablename__ = "role"
+
     id: int = Column(Integer, primary_key=True, autoincrement=True)
     title: str = Column(String, nullable=False, index=True)
     permissions: List = Column(List, default=[], nullable=False)
