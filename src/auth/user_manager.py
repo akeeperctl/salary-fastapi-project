2bf966d1b8d5944eab855291e5d7f1e7d19b7c0d
Daniel <d.akipskiy@gmail.com>
2023-06-22 16:06:25 +0800

    Add user manager

---
+++ src/auth/user_manager.py
@@ -0,0 +1,50 @@
+from typing import Optional, Union
+
+from fastapi import Depends, Request
+from fastapi_users import BaseUserManager, IntegerIDMixin, InvalidPasswordException
+
+from src.config import SECRET, TOKEN_RESET_PASSWORD_LIFETIME, TOKEN_VERIFICATION_LIFETIME
+from src.database import get_user_db
+from src.auth.models import Employee
+
+
+class UserManager(IntegerIDMixin, BaseUserManager[Employee, int]):
+    reset_password_token_secret = SECRET
+    reset_password_token_lifetime_seconds = TOKEN_RESET_PASSWORD_LIFETIME
+    verification_token_secret = SECRET
+    verification_token_lifetime_seconds = TOKEN_VERIFICATION_LIFETIME
+
+    async def validate_password(
+        self,
+        password: str,
+        user: Union[UserCreate, Employee],
+    ) -> None:
+        if len(password) < 8:
+            raise InvalidPasswordException(
+                reason="Password should be at least 8 characters"
+            )
+        if user.email in password:
+            raise InvalidPasswordException(
+                reason="Password should not contain e-mail"
+            )
+        if (user.username in password) or (user.firstname in password) or (user.lastname in password):
+            raise InvalidPasswordException(
+                reason="Password should not contain username/firstname/lastname"
+            )
+
+    async def on_after_register(self, user: Employee, request: Optional[Request] = None):
+        print(f"SERVER: User {user.id} has registered.")
+
+    async def on_after_forgot_password(
+            self, user: Employee, token: str, request: Optional[Request] = None
+    ):
+        print(f"SERVER: User {user.id} has forgot their password. Reset token: {token}")
+
+    async def on_after_request_verify(
+            self, user: Employee, token: str, request: Optional[Request] = None
+    ):
+        print(f"SERVER: Verification requested for user {user.id}. Verification token: {token}")
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
