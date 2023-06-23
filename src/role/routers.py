ce984f4f2bba66ce90f6776a455a85b63ce62a33
Daniel <d.akipskiy@gmail.com>
2023-06-23 10:40:28 +0800

    Mark role routers as deprecated

--- src/role/routers.py
+++ src/role/routers.py
@@ -1,67 +1,248 @@
-from typing import Union
-
-from sqlalchemy import insert, select, update
+from asyncpg import UniqueViolationError
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy import insert, select, update, delete
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
-from fastapi import APIRouter, Depends
-
-from src.database import get_async_session
+from response import ShiftAPIResponse
+from src.database import get_async_session, store_exact_data_from_db, store_data_from_db
 from src.role.models import Role as RoleModel
-from src.role.schemas import RoleCreate as RoleCreateScheme, RoleUpdate
+from src.role.schemas import RoleCreate as RoleCreateScheme, RoleUpdate, RoleRead
 
 role_router = APIRouter(
-    prefix="/role",
-    tags=["role"]
+    prefix="/roles",
+    tags=["roles"],
+    deprecated=True
 )
 
 
 @role_router.post(path="/add")
 async def create_role(role: RoleCreateScheme, session: AsyncSession = Depends(get_async_session)):
-    stmt = insert(RoleModel).values(title=role.title, permissions=role.permissions)
-    await session.execute(stmt)
-    await session.commit()
 
-    query = select(RoleModel).where(RoleModel.title == role.title)
+    role_title = role.title.lower()
+
+    try:
+        stmt = insert(RoleModel).values(title=role_title, permissions=role.permissions)
+        await session.execute(stmt)
+        await session.commit()
+    except (IntegrityError, UniqueViolationError):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=jsonable_encoder(
+                ShiftAPIResponse(
+                    status="error",
+                    description=f"role with title: {role_title} already created",
+                    data=None
+                )
+            )
+        )
+
+    query = select(RoleModel).where(RoleModel.title == role_title)
     result = await session.execute(query)
     new_role: RoleModel = result.scalar()
 
-    if new_role.title == role.title:
-        return {
-            "status": "success",
-            "description": None,
-            "data": new_role
-        }
+    if new_role.title.lower() == role_title:
+        raise HTTPException(
+            status_code=status.HTTP_201_CREATED,
+            detail=jsonable_encoder(
+                ShiftAPIResponse(
+                    status="success",
+                    description=f"role with id: {new_role.id} created",
+                    data=jsonable_encoder(new_role)
+                )
+            )
+        )
     else:
-        return {
-            "status": "error",
-            "description": None,
-            "data": None
-        }
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=jsonable_encoder(
+                ShiftAPIResponse(
+                    status="error",
+                    description=f"role with id: {new_role.id} not created",
+                    data=None
+                )
+            )
+        )
 
 
-@role_router.post(path="/{input_role_id}/update")
-async def update_role(role: RoleUpdate, input_role_id: int, session: AsyncSession = Depends(get_async_session)):
+@role_router.patch(path="/{role_id}")
+async def update_role(role: RoleUpdate, role_id: int, session: AsyncSession = Depends(get_async_session)):
+    stored_data = await store_exact_data_from_db(
+        row_id=role_id,
+        base_model=RoleModel,
+        base_read=RoleRead,
+        session=session
+    )
+
+    if stored_data is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=jsonable_encoder(
+                ShiftAPIResponse(
+                    status="error",
+                    description=f"role with id: {role_id} not found",
+                    data=None
+                )
+            )
+        )
+
+    # query = select(RoleModel).where(RoleModel.id == role_id)
+    # result = await session.execute(query)
+    # await session.commit()
+    # stored_job: RoleRead = result.scalar()
+    # stored_data: dict = jsonable_encoder(stored_job)
+
     stmt = update(RoleModel). \
-        where(RoleModel.id == input_role_id). \
-        values(**role.dict())
+        where(RoleModel.id == role_id). \
+        values(**role.dict(exclude_unset=True))
 
     await session.execute(stmt)
     await session.commit()
 
-    query = select(RoleModel).where(RoleModel.id == input_role_id)
-    result = await session.execute(query)
-    updated_role: RoleUpdate = result.scalar()
+    # query = select(RoleModel).where(RoleModel.id == role_id)
+    # result = await session.execute(query)
+    # updated_job: RoleRead = result.scalar()
+    # updated_data: dict = jsonable_encoder(updated_job)
 
-    if updated_role.title == role.title and \
-            updated_role.permissions == role.permissions:
-        return {
-            "status": "success",
-            "description": None,
-            "data": updated_role
-        }
+    updated_data = await store_exact_data_from_db(
+        row_id=role_id,
+        base_model=RoleModel,
+        base_read=RoleRead,
+        session=session
+    )
+
+    if not stored_data == updated_data:
+        raise HTTPException(
+            status_code=status.HTTP_200_OK,
+            detail=jsonable_encoder(
+                ShiftAPIResponse(
+                    status="success",
+                    description=f"role with id: {role_id} updated",
+                    data=None
+                )
+            )
+        )
     else:
-        return {
-            "status": "error",
-            "description": None,
-            "data": None
-        }
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=jsonable_encoder(
+                ShiftAPIResponse(
+                    status="error",
+                    description="stored data equal updated data",
+                    data=None
+                )
+            )
+        )
+
+
+@role_router.delete(path="/{role_id}")
+async def delete_job(role_id: int, session: AsyncSession = Depends(get_async_session)):
+    pre_delete_data = await store_exact_data_from_db(
+        row_id=role_id,
+        base_model=RoleModel,
+        base_read=RoleRead,
+        session=session
+    )
+
+    if pre_delete_data is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=jsonable_encoder(
+                ShiftAPIResponse(
+                    status="error",
+                    description=f"role with id: {role_id} not found",
+                    data=None
+                )
+            )
+        )
+
+    stmt = delete(RoleModel).where(RoleModel.id == role_id)
+    await session.execute(stmt)
+    await session.commit()
+
+    stored_data = await store_exact_data_from_db(
+        row_id=role_id,
+        base_model=RoleModel,
+        base_read=RoleRead,
+        session=session
+    )
+
+    if stored_data is None:
+        raise HTTPException(
+            status_code=status.HTTP_200_OK,
+            detail=jsonable_encoder(
+                ShiftAPIResponse(
+                    status="success",
+                    description=f"role with id: {role_id} deleted",
+                    data=None
+                )
+            )
+        )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_202_ACCEPTED,
+            detail=jsonable_encoder(
+                ShiftAPIResponse(
+                    status="error",
+                    description=f"role with id: {role_id} not deleted",
+                    data=None
+                )
+            )
+        )
+
+
+@role_router.get(path="/{role_id}")
+async def get_role(role_id: int, session: AsyncSession = Depends(get_async_session)):
+    stored_data = await store_exact_data_from_db(
+        row_id=role_id,
+        base_model=RoleModel,
+        base_read=RoleRead,
+        session=session
+    )
+
+    if stored_data is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=jsonable_encoder(
+                ShiftAPIResponse(
+                    status="error",
+                    description=f"role with id: {role_id} not found",
+                    data=None
+                )
+            )
+        )
+
+    raise HTTPException(
+        status_code=status.HTTP_200_OK,
+        detail=jsonable_encoder(
+            ShiftAPIResponse(
+                status="success",
+                description="",
+                data=stored_data
+            )
+        )
+    )
+
+
+@role_router.get(path="/all/")
+async def get_roles(limit: int, offset: int, session: AsyncSession = Depends(get_async_session)):
+    stored_data = await store_data_from_db(
+        limit=limit,
+        offset=offset,
+        base_model=RoleModel,
+        base_read=RoleRead,
+        session=session
+    )
+
+    raise HTTPException(
+            status_code=status.HTTP_200_OK,
+            detail=jsonable_encoder(
+                ShiftAPIResponse(
+                    status="success",
+                    description="",
+                    data=stored_data
+                )
+            )
+        )
