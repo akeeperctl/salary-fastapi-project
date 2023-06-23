a940f8489ad10c6ab99a4eff459eb60957da0692
Daniel <d.akipskiy@gmail.com>
2023-06-23 10:41:35 +0800

    Fix typos in auth modules

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,8 +1,8 @@
 import redis.asyncio
 from fastapi_users.authentication import BearerTransport, RedisStrategy, AuthenticationBackend
-from config import REDIS_HOST, REDIS_PORT, REDIS_STRATEGY_LIFETIME
+from src.config import REDIS_HOST, REDIS_PORT, REDIS_STRATEGY_LIFETIME
 
-bearer_transport = BearerTransport(tokenUrl="auth/rediska/login")
+bearer_transport = BearerTransport(tokenUrl="auth/login")
 
 # Redis используется для хранения токенов в формате словарика {user.id:token}
 redis = redis.asyncio.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}", decode_responses=True)
@@ -13,7 +13,7 @@ def get_redis_strategy() -> RedisStrategy:
 
 
 auth_backend = AuthenticationBackend(
-    name="rediska",
+    name="backend",
     transport=bearer_transport,
     get_strategy=get_redis_strategy,
 )
